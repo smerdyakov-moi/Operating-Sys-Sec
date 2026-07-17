c0e2bbea9ee0428901ad5dfa265e6b6f4997cbea
Pragyan Khati <pragyankhati45@gmail.com>
2026-07-17 09:44:02 +0545

    Add limiters to prevent input overflow leaks

--- Task - 3/securefilesys.c
+++ Task - 3/securefilesys.c
@@ -4,6 +4,12 @@
 #include<time.h>
 #include<stdbool.h>
 
+// Limiting string sizes to prevent input overflow
+#define MAX_CONTENT 512
+#define MAX_USERS 10
+#define MAX_FILES 20
+#define MAX_STR  32
+
 //Structure detailing permissions holding basic true/false flags for file access
 struct Permission{
     bool read;
@@ -16,6 +22,14 @@ struct FilePermission{
     struct Permission owner;
     struct Permission group;
     struct Permission others;
+};
+
+// Holding user account data
+struct User{
+    int user_id;
+    int group_id;
+    char username[MAX_STR];
+    char password[MAX_STR];
 };
 
 
