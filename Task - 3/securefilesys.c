9c8647da9bb1495ef7943ec9686a711413a18760
Pragyan Khati <pragyankhati45@gmail.com>
2026-07-17 09:58:12 +0545

    VirtualFile Struct Creation along with typedef introduction to avoid repetitive struct

--- Task - 3/securefilesys.c
+++ Task - 3/securefilesys.c
@@ -11,27 +11,44 @@
 #define MAX_STR  32
 
 //Structure detailing permissions holding basic true/false flags for file access
-struct Permission{
+typedef struct {
+
     bool read;
     bool write;
     bool execute;
-};
+
+} Permission;
 
 //Grouping permissions into 3 identities: Owner (Primarily root priviliges), Group and Others   
-struct FilePermission{
-    struct Permission owner;
-    struct Permission group;
-    struct Permission others;
-};
+typedef struct {
+
+    Permission owner;
+    Permission group;
+    Permission others;
+
+} FilePermission;
 
 // Holding user account data
-struct User{
+typedef struct {
+
     int user_id;
     int group_id;
     char username[MAX_STR];
     char password[MAX_STR];
-};
 
+} User;
+
+typedef struct {
+
+    char filename[MAX_STR];
+    char content[MAX_CONTENT];
+    int size;
+    int owner_id;
+    int group_id;
+    FilePermission perm;
+    bool isEncrypted; // Flagging true only if the file's encrypted
+
+} VirtualFile;
 
 int main(){
 
