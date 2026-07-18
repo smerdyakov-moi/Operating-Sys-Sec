04fcb38b64fa2ca0e9b6a805d5b6d87dd1b31219
Pragyan Khati <pragyankhati45@gmail.com>
2026-07-18 09:31:10 +0545

    Setting up Server/Owner Endpoint Socket

--- Task - 4/server.c
+++ Task - 4/server.c
@@ -7,11 +7,35 @@
 #include <netinet/in.h>
 
 #define PORT 4000
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 512 //setting the buffer size for incoming messages
 
 int main(){
+    
     struct sockaddr_in address; // Used sockaddr_in because it's specifically designed for handling IPV4
     // addresses and ports
-    int server_fd, client_socket;
+    int server_fd; // File descriptor tracking server's LISTENING master socket
+    int client_socket; // File descriptor tracking nearly spawned socket for a connected client
+    int opt = 1;
+    socklen_t addrlen = sizeof(address); 
+    char buffer[BUFFER_SIZE] = {0}; // Array buffer to read incoming network information
+
+    printf("1. Initializing Server Network....\n\n");
+
+    /*
+        socket() asks the kernel to build an 'endpoint' for network communication.
+        AF_INET: IPV4 standard system 4-octet IP address
+        SOCK_STREAM: Provides reliale sequenced **2-way** connection based byte streams
+
+    */
+
+    server_fd = socket(AF_INET,SOCK_STREAM,0);
+    if (server_fd < 0){
+        perror("Socket creation failed! \n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Socket successfully created inside OS kernel (FD number: %d) !\n\n",server_fd);
+
+    
 
 }
